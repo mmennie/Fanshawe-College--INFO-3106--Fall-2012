130cf188aea0ae6dfe2a3fe621ce0ef029e2a31f
Aaron McGowan <me@amcgowan.ca>
2012-09-14 12:35:18 -0400

    Added second argument to print_ln func

--- weeks/2/functions.php
+++ weeks/2/functions.php
@@ -10,12 +10,17 @@ error_reporting(E_ALL | E_STRICT);
  *
  * @access public
  * @param string Contains the string to print
+ * @param bool Determines whether a new line is to be used.
  * @return void
  */
-function print_ln($line)
+function print_ln($line, $ln_break)
 {
-  print $line . '<br />';
+  print $line;
+  if( $ln_break )
+  {
+    print '<br />';
+  }
 }
 
 
-print_ln('Hello World');
+print_ln('Hello World', true);
