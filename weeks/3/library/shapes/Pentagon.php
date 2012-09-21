171d16ab3d3eba345bd6e5976cd1323c3d632046
Aaron McGowan <me@amcgowan.ca>
2012-09-21 10:06:25 -0400

    Updates to shape classes

--- weeks/3/library/shapes/Pentagon.php
+++ weeks/3/library/shapes/Pentagon.php
@@ -5,15 +5,23 @@ defined('IN_APPLICATION') or exit;
 require_once 'Shape.php';
 
 class Pentagon extends Shape {
-  public function __construct() {
-    parent::__construct();
-  }
-  
   public function getName() {
     return 'Pentagon';
   }
   
   public function getNumberOfSides() {
     return 5;
+  }
+  
+  public function setDimensions(array $dimensions) {
+	$dimensions_count = count($dimensions);
+	if( 5 != $dimensions_count ) {
+		throw new Exception('You must specify exactly all five dimensions for the pentagon.');
+	}
+	
+	$this->_dimensions = array();
+	for( $i = 0; $i < $this->getNumberOfDimensions(); ++$i ) {
+		$this->_dimensions[] = $dimensions[$i];
+	}
   }
 }
--- weeks/3/library/shapes/Triangle.php
+++ weeks/3/library/shapes/Triangle.php
@@ -5,15 +5,29 @@ defined('IN_APPLICATION') or exit;
 require_once 'Shape.php';
 
 class Triangle extends Shape {
-  public function __construct() {
-    parent::__construct();
-  }
-  
   public function getName() {
     return 'Triangle';
   }
   
   public function getNumberOfSides() {
     return 3;
+  }
+  
+  public function setDimensions(array $dimensions) {
+	$dimensions_count = count($dimensions);
+	if( 2 > $dimensions_count || 3 < $dimensions_count ) {
+		throw new Exception('Triangle dimensions array must have either two or three elements.');
+	}
+	
+	if( 2 == $dimensions_count ) {
+		$this->_dimensions = array( $dimensions[0], $dimensions[1] );
+		$this->_dimensions[3] = sqrt( pow($this->_dimensions[0], 2) + pow($this->_dimensions[1], 2));
+	}
+	else {
+		$this->_dimensions = array();
+		for( $i = 0; $i < $this->getNumberOfDimensions(); ++$i ) {
+			$this->_dimensions[] = $dimensions[$i];
+		}
+	}
   }
 }
